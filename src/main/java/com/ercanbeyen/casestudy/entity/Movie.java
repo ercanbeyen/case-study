2d88d9dd8fb289e00037e2d313409288a1323019
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2023-05-08 10:11:02 +0300

    [TicketNo:] DTS0004 [Description:] Add Movie Repository. [Binary Source:] NA

--- src/main/java/com/ercanbeyen/casestudy/entity/Movie.java
+++ src/main/java/com/ercanbeyen/casestudy/entity/Movie.java
@@ -2,19 +2,38 @@ package com.ercanbeyen.casestudy.entity;
 
 import com.ercanbeyen.casestudy.constant.Genre;
 import com.ercanbeyen.casestudy.constant.Type;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 
-
-@Setter
-@Getter
-@SuperBuilder(toBuilder = true)
-//@Entity //Persistent entity 'Movie' should have primary key
-public class Movie extends WatchingItem {
-    public Movie(String imdbID, String title, String year, String rated, LocalDate released, Integer runtime, List<Genre> genres, String directorName, List<String> writers, String plot, List<String> languages, List<String> countries, String awards, String posterUrl, Integer metascore, Double imdbRating, Double imdbVotes, Type type, Boolean comingSoon) {
-        super(imdbID, title, year, rated, released, runtime, genres, directorName, writers, plot, languages, countries, awards, posterUrl, metascore, imdbRating, imdbVotes, type, comingSoon);
-    }
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Movie implements Serializable {
+    private String imdbID; // Primary Key
+    private String title;
+    private String year;
+    protected String rated;
+    @JsonFormat(pattern = "d MMM yyyy")
+    private LocalDate released;
+    private Integer runtime;
+    private List<Genre> genres;
+    private String directorName;
+    private List<String> writers;
+    private String plot;
+    private List<String> languages;
+    private List<String> countries;
+    private String awards;
+    private String posterUrl;
+    private Integer metascore;
+    private Double imdbRating;
+    private Double imdbVotes;
+    private Type type;
+    private Boolean comingSoon;
+    private Integer totalSeasons;
+    //protected Optional<Boolean> comingSoon;
 }
---
+++ src/main/java/com/ercanbeyen/casestudy/repository/impl/MovieRepositoryImpl.java
@@ -0,0 +1,105 @@
+package com.ercanbeyen.casestudy.repository.impl;
+
+import com.ercanbeyen.casestudy.constant.Type;
+import com.ercanbeyen.casestudy.entity.Movie;
+import com.ercanbeyen.casestudy.exception.EntityNotFound;
+import com.ercanbeyen.casestudy.repository.MovieRepository;
+import com.ercanbeyen.casestudy.util.FileHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class MovieRepositoryImpl implements MovieRepository, CommandLineRunner {
+    private static List<Movie> movieList;
+
+    @Override
+    public void run(String... args) throws Exception {
+        movieList = FileHandler.readFile();
+    }
+
+    public Long count() {
+        return (long) movieList.size();
+    }
+
+    public List<Movie> findAll() {
+        //movieList = FileHandler.readFile();
+        return movieList;
+    }
+
+    public Optional<Movie> findById(String id) {
+        //movieList = FileHandler.readFile();
+
+        return movieList.stream()
+                .filter(watchingItem -> watchingItem.getImdbID().equals(id))
+                .findFirst();
+    }
+
+    public void save(Movie movie) {
+        String id = movie.getImdbID();
+
+        if (!existsById(id)) {
+            movieList.add(movie);
+        } else {
+            for (Movie movieInDb : movieList) {
+                if (movieInDb.getType() == Type.SERIES) {
+                    movieInDb.setTotalSeasons(movie.getTotalSeasons());
+                }
+
+                if (movieInDb.getImdbID().equals(id)) {
+                    movieInDb.setTitle(movie.getTitle());
+                    movieInDb.setYear(movie.getYear());
+                    movieInDb.setReleased(movie.getReleased());
+                    movieInDb.setGenres(movie.getGenres());
+                    movieInDb.setAwards(movie.getAwards());
+                    movieInDb.setCountries(movie.getCountries());
+                    movieInDb.setLanguages(movie.getLanguages());
+                    movieInDb.setRuntime(movie.getRuntime());
+                    movieInDb.setComingSoon(movie.getComingSoon());
+                    movieInDb.setPlot(movie.getPlot());
+                    movieInDb.setRated(movie.getRated());
+                    movieInDb.setDirectorName(movie.getDirectorName());
+                    movieInDb.setWriters(movieInDb.getWriters());
+                    movieInDb.setImdbRating(movieInDb.getImdbRating());
+                    movieInDb.setImdbVotes(movie.getImdbVotes());
+                    movieInDb.setImdbVotes(movie.getImdbVotes());
+                    movieInDb.setPosterUrl(movie.getPosterUrl());
+                    movieInDb.setMetascore(movie.getMetascore());
+                    break;
+                }
+            }
+        }
+
+        FileHandler.writeFile(movieList);
+    }
+
+    public void deleteById(String id) {
+        Optional<Movie> watchingItem = movieList.stream()
+                .filter(item -> item.getImdbID().equals(id))
+                .findFirst();
+
+        if (!watchingItem.isPresent()) {
+            throw new EntityNotFound("Item" + id + "is not found");
+        }
+
+        movieList.remove(watchingItem.get());
+
+        if (existsById(id)) {
+            log.error("Item {} is not deleted from the database", id);
+            return;
+        }
+
+        FileHandler.writeFile(movieList); // delete the object and rewrite the updated list into the file
+    }
+
+    public boolean existsById(String id) {
+        return movieList.stream()
+                .anyMatch(watchingItem -> watchingItem.getImdbID().equals(id));
+    }
+}
