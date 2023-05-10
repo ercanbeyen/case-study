bf955e59246b85601ddd2cfaeb0cf2d599cc4a54
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2023-05-10 08:37:16 +0300

    [TicketNo:] DTS0007 [Description:] Add validations into Movie entity. [Binary Source:] NA

--- src/main/java/com/ercanbeyen/casestudy/exception/ExceptionResponse.java
+++ src/main/java/com/ercanbeyen/casestudy/exception/ExceptionResponse.java
@@ -10,5 +10,5 @@ import java.time.LocalDateTime;
 public class ExceptionResponse {
     private int status;
     private LocalDateTime localDateTime;
-    private String message;
+    private Object message;
 }
